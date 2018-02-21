9846c3a326ed7ff6ace581de8e9e1968b7635241
Damien Renaud <renauddamien86@gmail.com>
2018-02-21 09:27:08 +0100

    Complétion des scripts d'import pour mongodb et elasticsearch

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,50 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+let calls = [];
 fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
       // TODO extract one line from CSV
+      calls.push({
+        "lat": data.lat,
+        "lng": data.lng,
+        "desc": data.desc,
+        "zip": data.zip,
+        "title": data.title,
+        "timeStamp": data.timeStamp,
+        "twp": data.twp,
+        "addr": data.addr,
+        "e": data.e,
+      })
     })
     .on('end', () => {
       // TODO insert data to ES
+      esClient.bulk(createBulkInsertQuery(calls), (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`Inserted ${resp.items.length} calls`);
+        esClient.close();
+      });
     });
+
+    function createBulkInsertQuery(calls) {
+      const body = calls.reduce((acc, call) => {
+        const { lat, lng,  desc, zip, title, timeStamp, twp, addr } = call;
+        acc.push({ index: { _index: 'call_index', _type: 'call' } })
+        acc.push({ 
+          location: {
+            lat, 
+            lng
+          },  
+          desc, 
+          zip, 
+          title,
+          timeStamp,
+          twp, 
+          addr 
+        })
+        return acc
+      }, []);
+    
+      return { body };
+    }
